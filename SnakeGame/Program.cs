e283cd930edbe17d67a63cdc3e358cc3877531c6
Filip <f_bata@utb.com>
2024-02-13 22:03:50 +0100

    Add project files.

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 16;
+            Console.WindowWidth = 32;
+
+            var screenWidth = Console.WindowWidth;
+            var screenHeight = Console.WindowHeight;
+
+            var rand = new Random();
+
+            var score = 5;
+            var gameover = 0;
+
+            var head = new Pixel(screenWidth / 2, screenHeight / 2, ConsoleColor.Red);
+
+            var xPosBody = new List<int>();
+            var yPosBody = new List<int>();
+
+            var xPosBerry = rand.Next(0, screenWidth);
+            var yPosBerry = rand.Next(0, screenHeight);
+
+            var time2 = DateTime.Now;
+
+            var movement = "RIGHT";
+            var buttonPressed = "no";
+
+
+            while (true)
+            {
+                Console.Clear();
+                if (head.XPos == screenWidth - 1 || head.XPos == 0 || head.YPos == screenHeight - 1 || head.YPos == 0)
+                {
+                    gameover = 1;
+                }
+                for (int i = 0; i < screenWidth; i++)
+                {
+                    Console.SetCursorPosition(i, 0);
+                    Console.Write("■");
+                }
+                for (int i = 0; i < screenWidth; i++)
+                {
+                    Console.SetCursorPosition(i, screenHeight - 1);
+                    Console.Write("■");
+                }
+                for (int i = 0; i < screenHeight; i++)
+                {
+                    Console.SetCursorPosition(0, i);
+                    Console.Write("■");
+                }
+                for (int i = 0; i < screenHeight; i++)
+                {
+                    Console.SetCursorPosition(screenWidth - 1, i);
+                    Console.Write("■");
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                if (xPosBerry == head.XPos && yPosBerry == head.YPos)
+                {
+                    score++;
+                    xPosBerry = rand.Next(1, screenWidth - 2);
+                    yPosBerry = rand.Next(1, screenHeight - 2);
+                }
+                for (int i = 0; i < xPosBody.Count(); i++)
+                {
+                    Console.SetCursorPosition(xPosBody[i], yPosBody[i]);
+                    Console.Write("■");
+                    if (xPosBody[i] == head.XPos && yPosBody[i] == head.YPos)
+                    {
+                        gameover = 1;
+                    }
+                }
+                if (gameover == 1)
+                {
+                    break;
+                }
+                Console.SetCursorPosition(head.XPos, head.YPos);
+                Console.ForegroundColor = head.ScreenColor;
+                Console.Write("■");
+                Console.SetCursorPosition(xPosBerry, yPosBerry);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("■");
+                DateTime time = DateTime.Now;
+                buttonPressed = "no";
+                while (true)
+                {
+                    time2 = DateTime.Now;
+                    if (time2.Subtract(time).TotalMilliseconds > 500) { break; }
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo toets = Console.ReadKey(true);
+                        if (toets.Key.Equals(ConsoleKey.UpArrow) && movement != "DOWN" && buttonPressed == "no")
+                        {
+                            movement = "UP";
+                            buttonPressed = "yes";
+                        }
+                        if (toets.Key.Equals(ConsoleKey.DownArrow) && movement != "UP" && buttonPressed == "no")
+                        {
+                            movement = "DOWN";
+                            buttonPressed = "yes";
+                        }
+                        if (toets.Key.Equals(ConsoleKey.LeftArrow) && movement != "RIGHT" && buttonPressed == "no")
+                        {
+                            movement = "LEFT";
+                            buttonPressed = "yes";
+                        }
+                        if (toets.Key.Equals(ConsoleKey.RightArrow) && movement != "LEFT" && buttonPressed == "no")
+                        {
+                            movement = "RIGHT";
+                            buttonPressed = "yes";
+                        }
+                    }
+                }
+                xPosBody.Add(head.XPos);
+                yPosBody.Add(head.YPos);
+                switch (movement)
+                {
+                    case "UP":
+                        head.YPos--;
+                        break;
+                    case "DOWN":
+                        head.YPos++;
+                        break;
+                    case "LEFT":
+                        head.XPos--;
+                        break;
+                    case "RIGHT":
+                        head.XPos++;
+                        break;
+                }
+                if (xPosBody.Count() > score)
+                {
+                    xPosBody.RemoveAt(0);
+                    yPosBody.RemoveAt(0);
+                }
+            }
+            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2);
+            Console.WriteLine("Game over, Score: " + score);
+            Console.SetCursorPosition(screenWidth / 5, screenHeight / 2 + 1);
+        }
+
+
+        class Pixel
+        {
+            public Pixel(int xPos, int yPos, ConsoleColor color)
+            {
+                XPos = xPos;
+                YPos = yPos;
+                ScreenColor = color;
+            }
+            public int XPos { get; set; }
+            public int YPos { get; set; }
+            public ConsoleColor ScreenColor { get; set; }
+        }
+    }
+}
+
